f23398c6366301773a5dce54d211c43df8eadb69
shivamst11 <shivamtripathist11@gmail.com>
2021-06-14 04:36:45 +0530

    adding database

--- table_creator.py
+++ table_creator.py
@@ -1,15 +1,3 @@
-import sqlite3
-
-connection=sqlite3.connect('data.db')
-cursor=connection.cursor()
-
-table_query1="CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY,username VARCHAR,password VARCHAR)"
-table_query2="CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY,name VARCHAR)"
-#INTEGER PRIMARY KEY is used as increamental id when new data inserted id increase by 1 automatacly
-
-cursor.execute(table_query1)
-cursor.execute(table_query2)
-
-connection.commit()
-connection.close()
+from db import db
+db.create_all()
 
